0bad56761e8637e2be427454471757262e6dab18
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-07-11 13:52:03 +0100

    worked on codedisplay...

--- src/Components/CodeDisplay.tsx
+++ src/Components/CodeDisplay.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 
 const CodeDisplay = () => {
   return (
-    <div className='code-display'>
-      CodeDisplay
+    <div className="code-display">
+      <div className="buttons">
+        <div className="button"></div>
+        <div className="button"></div>
+        <div className="button"></div>
       </div>
+      <div className="code-output">
+        <p></p>
+      </div>
+    </div>
   )
 }
 
