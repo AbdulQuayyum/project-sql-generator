84f1c927dd28b31e3dc559e4a589769562bdc313
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-07-11 15:29:03 +0100

    working on the erver side..

---
+++ Index.ts
@@ -0,0 +1,38 @@
+import express from 'express'
+import cors from 'cors'
+import { Configuration, OpenAIApi } from 'openai'
+const PORT = 8080
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const dummy = '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'
+
+const configuration = new Configuration({
+  apiKey: 'dummy',
+})
+
+const openai = new OpenAIApi(configuration)
+
+app.post('/api', async (req, res) => {
+  try {
+    const completion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: 'Create a SQL request to ' + req.body.message,
+      temperature: 0.3,
+      max_tokens: 60,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    })
+    res.send(200).json
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .send(error?.response.data.error.message || 'Something went wrong')
+  }
+})
+
+app.listen(PORT, () => console.log('Connected'))
